e7f984c8485ed102c9685f269d9db3a61962e950
fredcoutinho52 <fredcoutinho52@gmail.com>
2022-01-30 17:50:15 -0300

    removendo sensor de peso

--- nivel_agua/nivel_agua.ino
+++ nivel_agua/nivel_agua.ino
@@ -1,22 +1,9 @@
-#include "HX711.h"
-
-#define DT A1
-#define SCK A0
-
-HX711 escala;
-
 const int pinoSensorAgua = A0;
 const int pinoBombaAgua = 12;
-const int pinoRacao = 11;
 
 void setup() {
-  escala.begin (DT, SCK);
-  escala.set_scale();
-  escala.tare(20);
-  
   pinMode(pinoSensorAgua, INPUT);
   pinMode(pinoBombaAgua, OUTPUT);
-  pinMode(pinoRacao, OUTPUT);
 }
 
 void loop() {
@@ -25,12 +12,5 @@ void loop() {
     digitalWrite(pinoBombaAgua, HIGH);
   } else {
     digitalWrite(pinoBombaAgua, LOW);
-  }
-
-  // VERIFICA O PESO DA RAÇÃO
-  if (escala.get_value(10) > 70000) {
-    digitalWrite(pinoRacao, HIGH);
-  } else {
-    digitalWrite(pinoRacao, LOW);
   }
 }

ff5a58cc9af4522b7af611ac372a7c284a2cc92e
fredcoutinho52 <fredcoutinho52@gmail.com>
2022-01-30 16:01:07 -0300

    sensor de peso

--- nivel_agua/nivel_agua.ino
+++ nivel_agua/nivel_agua.ino
@@ -1,15 +1,36 @@
-const int pinoSensor = A0;
-const int pinoLED = 12;
+#include "HX711.h"
+
+#define DT A1
+#define SCK A0
+
+HX711 escala;
+
+const int pinoSensorAgua = A0;
+const int pinoBombaAgua = 12;
+const int pinoRacao = 11;
 
 void setup() {
-  pinMode(pinoSensor, INPUT);
-  pinMode(pinoLED, OUTPUT);
+  escala.begin (DT, SCK);
+  escala.set_scale();
+  escala.tare(20);
+  
+  pinMode(pinoSensorAgua, INPUT);
+  pinMode(pinoBombaAgua, OUTPUT);
+  pinMode(pinoRacao, OUTPUT);
 }
 
 void loop() {
-  if(analogRead(pinoSensor) > 690) {
-    digitalWrite(pinoLED, HIGH);
+  // VERIFICA O NÍVEL DE ÁGUA
+  if(analogRead(pinoSensorAgua) > 690) {
+    digitalWrite(pinoBombaAgua, HIGH);
   } else {
-    digitalWrite(pinoLED, LOW);
+    digitalWrite(pinoBombaAgua, LOW);
+  }
+
+  // VERIFICA O PESO DA RAÇÃO
+  if (escala.get_value(10) > 70000) {
+    digitalWrite(pinoRacao, HIGH);
+  } else {
+    digitalWrite(pinoRacao, LOW);
   }
 }
